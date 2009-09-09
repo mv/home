ff4e1f5e708b929aae8065ef53bdfadf7e519a37
Marcus Vinicius Ferreira <marcus.ferreira@abril.com.br>
2009-09-09 15:47:39 -0300

    alias to oracle proxy script

--- bin/oracle.rc.sh
+++ bin/oracle.rc.sh
@@ -237,6 +237,14 @@ alias sysoper='sqlplus / as sysoper'
     alias    pmon='ps -ef | egrep [sp]mon'
 ## http://laurentschneider.blogspot.com/2006/05/ocm-preparation.html #}
 
+## mvdba proxy user {
+alias  abdh1='~/bin/mvdba.sh ~/config/ora-mvdba-abdh1.txt'
+alias  abdh2='~/bin/mvdba.sh ~/config/ora-mvdba-abdh2.txt'
+alias abdps1='~/bin/mvdba.sh ~/config/ora-mvdba-abdps1.txt'
+alias abdps2='~/bin/mvdba.sh ~/config/ora-mvdba-abdps2.txt'
+alias  dev01='~/bin/mvdba.sh ~/config/ora-system-dev01.txt'
+# }
+
 alias  ps1="export PS1='\u@\h:\w\n\$ '"
 export PS1='\[\e[01;33m\]\u\[\e[01;37m\]@\[\e[01;36m\]\h\[\e[01;37m\]:\[\e[00;33m\]\w\[\e[0m\] $(__git_ps1 "(%s)") \[\e[01;31m\]$(__ora_ps1)\[\e[0m\]\n\$ '
 
