97d1fc316d242087c4e2af8d5cb71a49725390c0
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-06-22 14:46:26 -0300

    Removido: set_window_and_tab_title

--- bin/bashrc.macosx.sh
+++ bin/bashrc.macosx.sh
@@ -14,35 +14,5 @@ alias sleep_combined='sudo pmset -a hibernatemode 3'
 
 alias rmdst='find . -type f -name .DS_Store | xargs rm -f'
 
-# MacOS Tab Name {
-function set_window_and_tab_title {
-    local title="$1"
-    if [[ -z "$title" ]]; then
-        title="root"
-    fi
-
-    local tmpdir=/tmp/cache/${FUNCNAME}_temp
-    local cmdfile="$tmpdir/$title"
-
-    # Set window title
-    echo -n -e "\e]0;${title}\a"
-
-    # Set tab title
-    if [[ -n ${CURRENT_TAB_TITLE_PID:+1} ]]; then
-        kill $CURRENT_TAB_TITLE_PID
-    fi
-    mkdir -p $tmpdir
-    ln /bin/sleep "$cmdfile"
-    "$cmdfile" 10 &
-    CURRENT_TAB_TITLE_PID=$(jobs -x echo %+)
-    disown %+
-    kill -STOP $CURRENT_TAB_TITLE_PID
-    command rm -f "$cmdfile"
-}
-
-PROMPT_COMMAND='set_window_and_tab_title "${PWD##*/}"'
-
-# }
-
 # vim: ft=sh:
 
