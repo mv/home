bb52ebd49566e5b1afde38ac9d41109b28c297c3
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-10-06 20:34:32 -0300

    More files

--- install.sh
+++ install.sh
@@ -27,6 +27,9 @@ lnfile ${DIR}/dot.gitk         ~/.gitk
 lnfile ${DIR}/dot.caprc        ~/.caprc
 lnfile ${DIR}/dot.hgrc         ~/.hgrc
 lnfile ${DIR}/dot.csshrc       ~/.csshrc
+lnfile ${DIR}/dot.my.cnf       ~/.my.cnf
+lnfile ${DIR}/dot.mytop        ~/.mytop
+lnfile ${DIR}/dot.screenrc     ~/.screenrc
 
 lndir  ${DIR}/bin              ~/bin
 
