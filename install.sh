1e7b1dbbd3fee525ad38122b0a43ec01b6f133a5
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-10-06 20:39:39 -0300

    More files

--- install.sh
+++ install.sh
@@ -19,18 +19,20 @@ lndir() {
 
 DIR=~/work/mv-home
 
-lnfile ${DIR}/dot.bashrc.sh    ~/.bashrc
-lnfile ${DIR}/dot.profile.sh   ~/.profile
-lnfile ${DIR}/dot.gitconfig    ~/.gitconfig
-lnfile ${DIR}/dot.gitignore    ~/.gitignore
-lnfile ${DIR}/dot.gitk         ~/.gitk
-lnfile ${DIR}/dot.caprc        ~/.caprc
-lnfile ${DIR}/dot.hgrc         ~/.hgrc
-lnfile ${DIR}/dot.csshrc       ~/.csshrc
-lnfile ${DIR}/dot.my.cnf       ~/.my.cnf
-lnfile ${DIR}/dot.mytop        ~/.mytop
-lnfile ${DIR}/dot.screenrc     ~/.screenrc
-lnfile ${DIR}/dot.inputrc      ~/.inputrc
+lnfile  ${DIR}/dot.bashrc.sh      ~/.bashrc
+lnfile  ${DIR}/dot.profile.sh     ~/.profile
+lnfile  ${DIR}/dot.gitattributes  ~/.gitattributes
+lnfile  ${DIR}/dot.gitconfig      ~/.gitconfig
+lnfile  ${DIR}/dot.gitignore      ~/.gitignore
+lnfile  ${DIR}/dot.gitk           ~/.gitk
+lnfile  ${DIR}/dot.caprc          ~/.caprc
+lnfile  ${DIR}/dot.hgrc           ~/.hgrc
+lnfile  ${DIR}/dot.csshrc         ~/.csshrc
+lnfile  ${DIR}/dot.my.cnf         ~/.my.cnf
+lnfile  ${DIR}/dot.mytop          ~/.mytop
+lnfile  ${DIR}/dot.screenrc       ~/.screenrc
+lnfile  ${DIR}/dot.inputrc        ~/.inputrc
+lnfile  ${DIR}/dot.irbrc          ~/.irbrc
 
 lndir  ${DIR}/bin              ~/bin
 
