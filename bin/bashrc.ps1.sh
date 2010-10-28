6c96c393a92537c7bf7c7a54a11b4a50b7d5114c
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-10-28 01:03:18 -0200

    ANSI colors + Reference

--- bin/bashrc.ps1.sh
+++ bin/bashrc.ps1.sh
@@ -9,6 +9,16 @@
 # Background color codes:
 #   40=black 41=red 42=green 43=yellow 44=blue 45=magenta 46=cyan 47=white
 
+# Ref: http://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+# Black       0;30     Dark Gray     1;30
+# Red         0;31     Light Red     1;31
+# Green       0;32     Light Green   1;32
+# Brown       0;33     Yellow        1;33
+# Blue        0;34     Light Blue    1;34
+# Purple      0;35     Light Purple  1;35
+# Cyan        0;36     Light Cyan    1;36
+# Light Gray  0;37     White         1;37
+
      red="\[\e[01;31m\]"
    green="\[\e[01;32m\]"
   yellow="\[\e[01;33m\]"
