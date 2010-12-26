fa10be723ae5540b9834957c3d7ca9c03fcb2d3e
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-26 15:31:52 -0200

    diff example

---
+++ bin/git-diff-wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###
+### git-diff-wrapper.sh
+###     Ref: http://technotales.wordpress.com/2009/05/17/git-diff-with-vimdiff/
+###
+### Marcus Vinicius Ferreira (Mv)               ferreira.mv[ at ]gmail.com
+### 2010/11
+###
+
+
+~/App/MacVim.app/Contents/MacOS/Vim -d "$2" "$5"
+
+
