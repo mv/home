490a3b3b8fedcf9ea8eafff91ee221a93068baec
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-26 23:23:47 -0200

    RVM prompt

--- bin/bashrc.ps1.sh
+++ bin/bashrc.ps1.sh
@@ -41,6 +41,7 @@ prompt="export PS1='${yellow}\\u${white}@${cyan}\\h${white}:${orange}\\w/"
 # type __ora_ps1   2>/dev/null 1>/dev/null && prompt="`echo $prompt ${red}``echo '$(__ora_ps1)'`"
 # type __mysql_ps1 2>/dev/null 1>/dev/null && prompt="`echo $prompt ${cyan}``echo '$(__mysql_ps1)'`"
   type __git_ps1   2>/dev/null 1>/dev/null && prompt="`echo $prompt ${green}``echo '$(__git_ps1 "(%s)")'`"
+  type __rvm_ps1   2>/dev/null 1>/dev/null && prompt="`echo $prompt ${red}``echo '$(__rvm_ps1)'`"
 
 # End colors and evaluate
 prompt="$prompt $reset\n\$ '"
--- bin/bashrc.rails.sh
+++ bin/bashrc.rails.sh
@@ -24,5 +24,18 @@ do
     alias $cmd="run_bundler_cmd $cmd"
 done
 
+### RVM
+#[[ -s "$HOME/.rvm/scripts/rvm"     ]] && source "$HOME/.rvm/scripts/rvm"       # This loads RVM into a shell session.
+ [[ -s "/usr/local/rvm/scripts/rvm" ]] && {
+
+    source "/usr/local/rvm/scripts/rvm"   # This loads RVM into a shell session.
+    rvm ree
+
+    function __rvm_ps1() {
+    [ "$rvm_ruby_string" ] && echo "[$rvm_ruby_string]"
+    }
+
+}
+
 # vim: ft=sh:
 
