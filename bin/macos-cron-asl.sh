6efdddb73fd73f3b2e1e35dec8556d520a0368d1
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2011-12-25 20:36:03 -0200

    Keep /var/log/asl/ to a mininum size

---
+++ bin/macos-cron-asl.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# cron-ash.sh
+#     Keep /var/log/asl/ to a mininum size
+#
+# 00 02  *  *  * /work/mv-home/bin/macos-cron-asl.sh -f
+#
+# Ref:
+#     http://boredzo.org/blog/archives/2008-01-19/next-week-apple-system-logger
+#     http://crucialsecurityblog.harris.com/tag/macintosh/
+#     http://developer.apple.com/library/mac/#documentation/Darwin/Reference/ManPages/man5/asl.conf.5.html
+#
+# Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
+# 2011-12
+#
+
+[ "$1" != "-f" ] && {
+    echo
+    echo "Usage: $0 -f"
+    echo
+    echo "    MacOS: To be used in root's cron"
+    echo
+    exit 1
+}
+
+PATH="/usr/local/bin:/usr/bin:/bin"
+
+LOG="/var/log/${0}.log"
+
+echo_do() {
+    echo "$@"
+    $@
+}
+
+# main: logging everything
+{
+    echo "Begin"
+    echo_do "launchctl stop  com.apple.syslogd"
+    echo_do "launchctl stop  com.apple.aslmanager"
+
+    for file in $( find /var/log/asl/ -type f -mtime +1 )
+    do
+        /bin/ls -lh $file
+        /bin/rm $file
+    done
+
+    echo_do "launchctl start com.apple.syslogd"
+    echo "End"
+
+} | gawk '{print strftime("%Y-%m-%d %H:%M:%S :", systime()), $0 }' \
+ >> $LOG
+
+# Auto-rotate
+size=$( /bin/ls -l $LOG | awk '{print $5}' ) # size bytes
+[ $size -gt 5242880 ] && > $LOG              # truncate when > 5000k, 5mb
+
+
+# vim:ft=sh:
+
