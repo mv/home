ceeb9f29de784e952c5ce6f18d56d0428702834c
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2012-01-11 11:11:29 -0200

    CERT, PRIVATE_KEY, USERID in the environment

--- bash.d/40-ec2.sh
+++ bash.d/40-ec2.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
+#
+# using ec2 tools
+#
+# Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
+# 2011-11
+
 
 ###
-### mvf
-### using ec2-ami-tools
+### programs
+###
+case $( uname -s ) in
+    Darwin)
+        # pre-req:
+        #     brew install ec2-ami-tools
+        #     brew install ec2-api-tools
+        #     mkdir ~/.ec2 && chmod 700 ~/.ec2
+        export        JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+        export         EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.0.1/jars"
+        export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
+    ;;
+    Linux)
+        # pre-req:
+        #     yum install openjdk-7.0
+        #     yum install ec2-api-tools
+        #     yum install ec2-ami-tools
+        export        JAVA_HOME="/usr"
+        export         EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.0.1/jars"
+        export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
+    ;;
+esac
 
-
-# pre-req:
-#     brew install ec2-ami-tools
-#     brew install ec2-api-tools
-#     mkdir ~/.ec2 && chmod 700 ~/.ec2
-
-export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.0.1/jars"
-export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
-export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
-
-function __ec2_user_id() {
-    cat $HOME/.ec2/ec2-user-id.txt
-}
+###
+### credentials
+###     mkdir ~/.ec2 && chmod 700 ~/.ec2
+###
+export        EC2_CERT="$(/bin/ls  $HOME/.ec2/cert-*.pem)"
+export EC2_PRIVATE_KEY="$(/bin/ls  $HOME/.ec2/pk-*.pem)"
+export     EC2_USER_ID="$(/bin/cat $HOME/.ec2/ec2-user-id.txt)"
 
 function __ec2_access_id() {
     cat $HOME/.ec2/ec2-access-id.txt
@@ -28,10 +46,9 @@ function __ec2_secret_key() {
     cat $HOME/.ec2/ec2-secret-key.txt
 }
 
-export    EC2_USER_ID="$( __ec2_user_id    )"
-export  EC2_ACCESS_ID="$( __ec2_access_id  )"
-export EC2_SECRET_KEY="$( __ec2_secret_key )"
-
+###
+### shortcuts
+###
 alias ec2-list-keypairs=' ec2-describe-keypairs | column -t | sort'
 alias ec2-list-regions='  ec2-describe-regions  | sort'
 alias ec2-list-tags='     ec2-describe-tags     | sort'
