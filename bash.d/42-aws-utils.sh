35776a7d08050d89379c1b599999e9dd50517cfe
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2013-08-01 17:16:07 -0300

    csshX tricks.

---
+++ bash.d/42-aws-utils.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Changing between amazon accounts
+#
+# This script must be sourced by your ~/.bashrc or something
+#
+# Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
+# 2013-08
+
+
+###
+### by security group
+###
+function csshg() {
+    [ -z "$1" ] && return
+
+    list=$( aws din --filter "group-name=$1" --filter "instance-state-code=16" --simple | awk '{print $3}' )
+
+    # debug
+    if [ "$2" == "-d" ]
+    then
+        echo "List:"
+        echo "[$list]"
+        return
+    fi
+
+    # $2: one extra server
+    csshX --login ec2-user $list $2
+
+}
+
+
+###
+### by tag 'Name'
+###
+function cssht() {
+    [ -z "$1" ] && return
+
+    list=$( aws din --filter "tag:Name=${1}*" --filter "instance-state-code=16" --simple | awk '{print $3}' )
+
+    # debug
+    if [ "$2" == "-d" ]
+    then
+        echo "List:"
+        echo "[$list]"
+        return
+    fi
+
+    # $2: one extra server
+    csshX --login ec2-user $list $2
+
+}
+
+
+# vim:ft=sh:
+
