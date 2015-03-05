22ae37901a2261ebf4777d9ca439cece8bc82946
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2015-03-05 16:40:00 -0300

    Relative to "~" now...

--- bash.d/41-awsenv.sh
+++ bash.d/41-awsenv.sh
@@ -6,11 +6,16 @@
 #
 # Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
 # 2012-11
+# 2015-03
 
-export  AWSENV_PROFILES_DIR="~/work/mv/mv-priv/awsenv/profiles"
-export AWSENV_TEMPLATES_DIR="~/work/mv/mv-priv/awsenv/templates" # optional
+eval  AWSENV_PROFILES_DIR="~/work/mv/mv-priv/awsenv/profiles"
+eval AWSENV_TEMPLATES_DIR="~/work/mv/mv-priv/awsenv/templates" # optional
+
+export AWSENV_PROFILES_DIR AWSENV_TEMPLATES_DIR
 
 source ~/work/mv/dev/mv-awsenv/awsenv.sh
+
+awsenv-set "ferreira.mv"
 
 # vim:ft=sh:
 
