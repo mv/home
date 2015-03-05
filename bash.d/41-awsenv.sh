69ee31a990353611944f021237d16a326d1a8b64
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2015-03-05 13:03:38 -0300

    Updated to '~/work/'

--- bash.d/41-awsenv.sh
+++ bash.d/41-awsenv.sh
@@ -7,12 +7,10 @@
 # Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
 # 2012-11
 
+export  AWSENV_PROFILES_DIR="~/work/mv/mv-priv/awsenv/profiles"
+export AWSENV_TEMPLATES_DIR="~/work/mv/mv-priv/awsenv/templates" # optional
 
-
-export  AWSENV_PROFILES_DIR="/work/mv-priv/awsenv/profiles"
-export AWSENV_TEMPLATES_DIR="/work/mv-priv/awsenv/templates" # optional
-
-source /work/dev/mv-awsenv/awsenv.sh
+source ~/work/mv/dev/mv-awsenv/awsenv.sh
 
 # vim:ft=sh:
 
