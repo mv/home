0e6cf09c164d84cbd76a03f627336432c6cda038
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2018-12-31 20:50:52 -0200

    Disable pycache dirs.

---
+++ bash.d/20-python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# vim: ft=sh:
+
+###
+### mvf
+### bashrc lib
+
+### Python
+
+# Disable __pycache__ dirs
+export PYTHONDONTWRITEBYTECODE=1
+
+
+# Tip: pip completion
+# pip completion --bash > ~/bash_completion.d/pip.bash-completion
+
