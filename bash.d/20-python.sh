ebf313288f013708be3d6f60b793a8a06843a86d
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2019-01-04 01:21:58 -0200

    Python: activate

--- bash.d/20-python.sh
+++ bash.d/20-python.sh
@@ -3,12 +3,21 @@
 
 ###
 ### mvf
-### bashrc lib
-
 ### Python
+
+# pyenv
+# https://github.com/pyenv/pyenv
+
+# export PYENV_ROOT=~/work/dev/pyenv
+# export PYENV_ROOT=~/.pyenv
+# eval "$(pyenv init -)"
 
 # Disable __pycache__ dirs
 export PYTHONDONTWRITEBYTECODE=1
+
+# virtualenv
+alias pyactivate="source .venv/bin/activate"
+alias pydeactivate="deactivate"
 
 
 # Tip: pip completion
