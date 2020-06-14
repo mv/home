02bb457954b3ea773d7e0aabc1c45d7bb7b35e7d
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-06-14 13:56:10 -0300

    Atom: testing...
    # Ref: https://gist.github.com/2697681
    #
    # A good commit message looks like this:
    # --------------------------------------
    #
    #     Header line: explaining the commit in one line
    #
    #     Body of commit message is a few lines of text, explaining things
    #     in more detail, possibly giving some background about the issue
    #     being fixed, etc etc.
    #
    #     The body of the commit message can be several paragraphs, and
    #     please do proper word-wrap and keep columns shorter than about
    #     74 characters or so. That way "git log" will show things
    #     nicely even when it's indented.
    #
    # where that header line really should be meaningful, and really should be
    # just one line.  That header line is what is shown by tools like gitk and
    # shortlog, and should summarize the change in one readable line of text,
    # independently of the longer explanation.
    #

---
+++ bash.d/20-atom.io.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# vim: ft=sh:
+
+###
+### mvf
+### https://atom.io/
+###
+
+# Home: my dropbox
+# export ATOM_HOME=~/Dropbox/app/atom
+
+# Tip: pip completion
+# pip completion --bash > ~/bash_completion.d/pip.bash-completion
+
