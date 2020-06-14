380ea624d26aacbb8ff0aaf3dc0c544bec2202e4
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-06-14 13:55:11 -0300

    Simplify after Vim 8.1
    # Ref: https://gist.github.com/2697681
    #
    # A good commit message looks like this:
    # --------------------------------------
    #
    #     Header line: explaining the commit in one line
    #
    #     Body of commit message is a few lines of text, explaining things
    #     in more detail, possibly giving some background about the issue
    #     being fixed, etc etc.
    #
    #     The body of the commit message can be several paragraphs, and
    #     please do proper word-wrap and keep columns shorter than about
    #     74 characters or so. That way "git log" will show things
    #     nicely even when it's indented.
    #
    # where that header line really should be meaningful, and really should be
    # just one line.  That header line is what is shown by tools like gitk and
    # shortlog, and should summarize the change in one readable line of text,
    # independently of the longer explanation.
    #

--- bash.d/20-vim.sh
+++ bash.d/20-vim.sh
@@ -7,9 +7,15 @@
 # Vim {
 export EDITOR=vim
 
+# alias  vim='vim  -u /Users/mvferr/work/mv-vim/dot.vimrc --startuptime /tmp/vim.time.1.txt'
+# alias gvim='gvim -u /Users/mvferr/work/mv-vim/dot.vimrc --startuptime /tmp/vim.time.1.txt'
+
+# alias  vim20='vim  -u /Users/mvferr/work/vim2020/vimrc'
+# alias gvim20='gvim -u /Users/mvferr/work/vim2020/vimrc'
+
 case `uname -s` in
     Darwin)
-        alias   vi='/usr/bin/vim'
+#       alias   vi='/usr/bin/vim'
 #       alias  vim='~/App/MacVim.app/Contents/MacOS/Vim'
         alias tvim='gvim --remote-tab 2>/dev/null'
 #       alias gvim='~/App/MacVim.app/Contents/MacOS/Vim -g'
@@ -17,7 +23,7 @@ case `uname -s` in
 
 #       export VIM_APP_DIR=~/App
 #       export MANPAGER="col -b | ~/bin/view  -c 'set ft=man nomod nolist' -"
-        export MANPAGER="col -b | ~/bin/mview -c 'set ft=man nomod nolist' -"
+#       export MANPAGER="col -b | ~/bin/mview -c 'set ft=man nomod nolist' -"
         ;;
     *)
         alias vi=vim
