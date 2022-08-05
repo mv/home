b102061b6cbdfc617056166fc1276ef8a9bc3bb0
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2022-08-05 10:13:08 -0300

    New: terraform

---
+++ bash.d/60-terraform.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# vim:ft=sh:
+#
+# Terraform
+#
+# Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
+#
+# 2022-08: Terraform 1.2.6
+
+###
+#     https://learn.hashicorp.com/tutorials/terraform/install-cli
+#     https://www.terraform.io/cli/config/environment-variables
+#
+#     $ brew install terraform
+#     $ terraform -install-autocomplete
+#
+
+export TF_LOG_PATH=./terraform.log
+export TF_LOG=off
+
+###
+### bash-completion
+###
+
+if which /usr/local/bin/terraform 2>/dev/null 1>/dev/null
+then complete -C /usr/local/bin/terraform terraform
+fi
