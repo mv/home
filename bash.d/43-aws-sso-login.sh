b59d041ed88de87ebec93984fb3222521d83ee07
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2023-02-02 11:42:53 -0300

    New: aws/sso
    # Ref: https://gist.github.com/2697681
    #
    # A good commit message looks like this:
    # --------------------------------------
    #
    #     Header line: explaining the commit in one line
    #
    #     Body of commit message is a few lines of text, explaining things
    #     in more detail, possibly giving some background about the issue
    #     being fixed, etc etc.
    #
    #     The body of the commit message can be several paragraphs, and
    #     please do proper word-wrap and keep columns shorter than about
    #     74 characters or so. That way "git log" will show things
    #     nicely even when it's indented.
    #
    # where that header line really should be meaningful, and really should be
    # just one line.  That header line is what is shown by tools like gitk and
    # shortlog, and should summarize the change in one readable line of text,
    # independently of the longer explanation.
    #

---
+++ bash.d/43-aws-sso-login.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# vim:ft=sh:
+#
+# Helper to `aws sso login`
+#   aws-sso-login <profile-name>
+#
+# Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
+# 2023-02
+#
+
+###
+###
+###
+
+function aws-sso-login() {
+
+  if [ "${1}" == "" ] 
+  then
+    echo "Usage: aws-sso-login <profile-name>"
+    echo
+    echo "  Reads SSO info from ~/.aws/config"
+    echo
+  else
+    aws sso login --profile="${1}"
+  fi
+
+}
+
