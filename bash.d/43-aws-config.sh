37908a79b1afbcdacb60a09008a2ce2339ff3e94
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2023-02-03 17:20:09 -0300

    Option '-l': reorder
    # Ref: https://gist.github.com/2697681
    #
    # A good commit message looks like this:
    # --------------------------------------
    #
    #     Header line: explaining the commit in one line
    #
    #     Body of commit message is a few lines of text, explaining things
    #     in more detail, possibly giving some background about the issue
    #     being fixed, etc etc.
    #
    #     The body of the commit message can be several paragraphs, and
    #     please do proper word-wrap and keep columns shorter than about
    #     74 characters or so. That way "git log" will show things
    #     nicely even when it's indented.
    #
    # where that header line really should be meaningful, and really should be
    # just one line.  That header line is what is shown by tools like gitk and
    # shortlog, and should summarize the change in one readable line of text,
    # independently of the longer explanation.
    #

--- bash.d/43-aws-config.sh
+++ bash.d/43-aws-config.sh
@@ -14,15 +14,15 @@
 
 function aws-config() {
 
-  if [ "${1}" == "" ]
-  then _section="${AWS_PROFILE}"
-  else _section="${1}"
-  fi
-
   if [ "${1}" == "-l" ]
   then 
     grep "^\[profile" ~/.aws/config | sort
     return 0
+  fi
+
+  if [ "${1}" == "" ]
+  then _section="${AWS_PROFILE}"
+  else _section="${1}"
   fi
 
   # awk: capture text between /regex-1/,/regex-2/
