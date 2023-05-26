b0d40592caba88652ad31903238c52cef1d8cae8
Marcus Vinicius Ferreira <marcus.ferreira@pismo.io>
2023-05-26 14:49:17 -0300

    Fix: whitespace

--- bash.d/43-aws-profile.sh
+++ bash.d/43-aws-profile.sh
@@ -2,7 +2,7 @@
 # vim:ft=sh:
 #
 # aws-profile [<profile-name>]
-#   View/change AWS_PROFILE  
+#   View/change AWS_PROFILE
 #
 # Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
 # 2023-02
@@ -15,18 +15,16 @@
 function aws-profile() {
 
   if [ "${1}" == "-l" ]
-  then 
+  then
     grep "^\[profile" ~/.aws/config | sort
     return 0
   fi
 
   if ! [ "${1}" == "" ]
-  then
-    export AWS_PROFILE="${1}"
+  then export AWS_PROFILE="${1}"
   fi
 
   echo
-  echo "AWS_PROFILE='${AWS_PROFILE}'" 
+  echo "AWS_PROFILE='${AWS_PROFILE}'"
   echo
 }
-
