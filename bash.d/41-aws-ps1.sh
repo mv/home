6605af52d0a0ca571ed16942fedcb5033455f9a0
Mv <ferreira.mv@gmail.com>
2023-10-20 09:51:42 -0300

    Added: show a non-default AWS_CONFIG_FILE

--- bash.d/41-aws-ps1.sh
+++ bash.d/41-aws-ps1.sh
@@ -13,6 +13,15 @@
 
 function __aws_ps1() {
 
+  # no quotes: expand tilde
+  _default=~/.aws/config
+
+  # show CONFIG_FILE if different from default
+  if [ "${AWS_CONFIG_FILE}" == "${_default}" ]
+  then _ps1=""
+  else _ps1="cfg:${AWS_CONFIG_FILE}:"
+  fi
+
   # awk 1: capture text between /regex-1/,/regex-2/
   # awk 2: separate k/v pair by '='
 
@@ -29,10 +38,12 @@ function __aws_ps1() {
   #
   # build the prompt:
   #
-  [ "${AWS_PROFILE}" ]  && _ps1="${AWS_PROFILE}"
+  [ "${AWS_PROFILE}" ]  && _ps1="${_ps1}prf:${AWS_PROFILE}"
   [ "${_aws_region}" ]  && _ps1="${_ps1}:${_aws_region}"
-  [ "${_aws_account}" ] && _ps1="${_ps1}:${_aws_account}"
+  [ "${_aws_account}" ] && _ps1="${_ps1}:sso:${_aws_account}"
+
 # echo "[aws:${_ps1}]"
   echo "[${_ps1}]"
+
 }
 
