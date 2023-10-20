23d3b56fe59b1ae1baf98541777ccab715a74edb
Mv <ferreira.mv@gmail.com>
2023-10-20 09:50:39 -0300

    Comments.

--- bash.d/20-java.sh
+++ bash.d/20-java.sh
@@ -2,5 +2,10 @@
 # vim: ft=sh:
 
 
-# export JAVA_TOOLS_OPTIONS="-Dlog4j2.formatMsgNoLookups=true"
+# Log4J vulnerability
+# https://logging.apache.org/log4j/2.x/security.html
+export LOG4J_FORMAT_MSG_NO_LOOKUPS=true
+
+#
+pathadd /opt/homebrew/opt/openjdk/bin
 
