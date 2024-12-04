fb12ef4cfbd958c37cbf088c39078c6dd3d79710
inoa-mvferreira <mvferreira@inoa.com.br>
2024-12-04 19:46:24 -0300

    More comments.

--- bash.d/99-ps1.sh
+++ bash.d/99-ps1.sh
@@ -42,12 +42,12 @@ marker="${prompt}"
 # type __mysql_ps1  &>/dev/null && prompt="${prompt} ${cyan}\$(__mysql_ps1)"
 # type __rvm_ps1    &>/dev/null && prompt="${prompt} ${red}\$(__rvm_ps1)"
   type __aws_ps1    &>/dev/null && prompt="${prompt} ${cyan}\$(__aws_ps1)"
-  type __venv_ps1   &>/dev/null && prompt="${prompt} ${yellow}\$(__venv_ps1)"
+  type __venv_ps1   &>/dev/null && prompt="${prompt}${yellow}\$(__venv_ps1)"
   type __git_ps1    &>/dev/null && prompt="${prompt}${green}\$(__git_ps1)"
 
+# Close (and adopt) dynamic prompt if any function was added
 if [ "${prompt}" != "${marker}" ]
 then
-  # Close (and adopt) dynamic prompt
   # \012 or \n
   # https://stackoverflow.com/questions/33712750/git-config-global-returns-syntax-error-near-unexpected-token-error
   export PS1="${prompt}${reset}\012\\$ "
