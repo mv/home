aef8a80a5a45fd69ce3689df8c06028fc9be6622
inoa-mvferreira <mvferreira@inoa.com.br>
2024-12-06 20:01:48 -0300

    Make flags between GNU and BSD explicit.

--- bash.d/10-ls.sh
+++ bash.d/10-ls.sh
@@ -4,32 +4,32 @@
 ### mvf
 ### bashrc lib
 
-# ls {
+_bsd='-AFhG'
+_gnu='-AFh --color=auto --time-style=long-iso --group-directories-first'
+
 case `uname -s` in
     Darwin | FreeBSD | OpenBSD)
-        alias ls='ls -AFhG'
+        alias ls="ls ${_bsd}"
         ;;
     Linux)
-        alias ls='ls -AFh --color=auto --time-style=long-iso --group-directories-first'
-        # bright colors: http://www.walkernews.net/2007/03/29/brighten-linux-ls-command-output-with-ls_colors/
-        # eval `echo $LS_COLORS | sed 's/00;/01;/g' | awk '{print "export LS_COLORS=\""$0"\""}' `
+        alias ls="ls ${_gnu}"
         ;;
     *)
         alias ls='ls -AF'
         ;;
 esac
 
-if which gls 2>/dev/null 1>/dev/null 
-then alias ls='gls -AFh --color=auto --time-style=long-iso --group-directories-first'
+# Homebrew
+if   which gls &>/dev/null 
+then alias ls="gls ${_gnu}"
 fi
 
-alias ll='ls -l'                       # long list
-alias lr='ls -ltr'                     # long list
-alias la='ls -A'                       # all but . and ..
-alias  l='gls --color'
+alias ll='ls -l'       # long list
+alias lr='ls -ltr'     # long list
+alias la='ls -A'       # all but . and ..
+alias  l='ls --color'
+
+# bright colors: http://www.walkernews.net/2007/03/29/brighten-linux-ls-command-output-with-ls_colors/
+# eval `echo $LS_COLORS | sed 's/00;/01;/g' | awk '{print "export LS_COLORS=\""$0"\""}' `
 export LS_COLORS='*sql=33:*log=93:*buf=31'
-# }
-
-
-# vim: ft=sh:
 
