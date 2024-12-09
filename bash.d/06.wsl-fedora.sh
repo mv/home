e331251cf044d48819eafe2a1e816b78724811a1
inoa-mvferreira <mvferreira@inoa.com.br>
2024-12-08 23:39:24 -0300

    Fix VSCode path to cli.

--- bash.d/06.wsl-fedora.sh
+++ bash.d/06.wsl-fedora.sh
@@ -5,21 +5,22 @@
 #
 # Marcus Vinicius Fereira            ferreira.mv[ at ].gmail.com
 
-#alias git='/mnt/c/Program\ Files/Git/mingw64/bin/git.exe'
 
-## set default browser to be open from terminal
+## WSL
+pathadd /home/linuxbrew/.linuxbrew/bin
+
+### set default browser to be open from terminal
 _choosy="/mnt/c/Program Files/BrowserPicker/BrowserPicker.exe"
 if [ -f "${_choosy}" ]
 then export BROWSER="${_choosy}"
 fi
 
+
 ## Git/Bash
 pathadd /mingw64/bin
+pathadd "/c/Program Files/Microsoft VS Code/bin"
+
+
+## Cygwin
 pathadd "/cygdrive/c/Program Files/Git/mingw64/bin"
-
-## VSCode
-pathadd "/mnt/c/Program Files/Microsoft VS Code/bin"
 pathadd "/cygdrive/c/Program Files/Microsoft VS Code/bin"
-
-## Homebrew/Linux
-pathadd /home/linuxbrew/.linuxbrew/bin
