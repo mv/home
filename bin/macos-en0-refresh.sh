ef30b5b9b43848ac8b2a80c518bdab4181ee8632
Mv <ferreira.mv@gmail.com>
2026-03-13 15:53:52 -0300

    Deprecated.

--- bin/macos-en0-refresh.sh
+++
@@ -1,5 +0,0 @@
-
-sudo ifconfig en0 down
-sudo ifconfig en0 up
-sudo ifconfig en0
-
--- old/ruby.my.gems.sh
+++
@@ -1,8 +0,0 @@
-
-gem install rake bundler
-gem install capistrano capistrano_colors
-gem install codebase github
-gem install pry awesome_print
-gem install vagrant
-gem install puppet facter
-
