9da7d6fcd32567f9b7e69e443b672d9cbed8e99b
Mv <ferreira.mv@gmail.com>
2026-04-17 17:23:30 -0300

    NeoVim.

--- bash.d/12-vim.sh
+++ bash.d/12-vim.sh
@@ -5,13 +5,20 @@
 ### mvf
 ### bashrc lib
 
-if _cmd_exists git
-then _bashrc_verbose "== Vim"
+if _cmd_exists nvim
+then
+    _bashrc_verbose "== Vim: NeoVim"
+    alias vi=nvim
+    export EDITOR=nvim
+elif _cmd_exists vim
+then
+    _bashrc_verbose "== Vim"
+    alias vi=vim
+    export EDITOR=vim
 else _bashrc_verbose "== Vim: NOT FOUND"
 fi
 
 # Vim {
-export EDITOR=vim
 
 # alias  vim='vim  -u /Users/mvferr/work/mv-vim/dot.vimrc --startuptime /tmp/vim.time.1.txt'
 # alias gvim='gvim -u /Users/mvferr/work/mv-vim/dot.vimrc --startuptime /tmp/vim.time.1.txt'
@@ -19,23 +26,40 @@ export EDITOR=vim
 # alias  vim20='vim  -u /Users/mvferr/work/vim2020/vimrc'
 # alias gvim20='gvim -u /Users/mvferr/work/vim2020/vimrc'
 
-case `uname -s` in
-    Darwin)
+# case `uname -s` in
+#   Darwin)
 #       alias   vi='/usr/bin/vim'
 #       alias  vim='~/App/MacVim.app/Contents/MacOS/Vim'
-        alias tvim='gvim --remote-tab 2>/dev/null'
+#       alias tvim='gvim --remote-tab 2>/dev/null'
 #       alias gvim='~/App/MacVim.app/Contents/MacOS/Vim -g'
 #       alias vimd='~/App/MacVim.app/Contents/MacOS/Vim -g -d'
-
+#
 #       export VIM_APP_DIR=~/App
 #       export MANPAGER="col -b | ~/bin/view  -c 'set ft=man nomod nolist' -"
 #       export MANPAGER="col -b | ~/bin/mview -c 'set ft=man nomod nolist' -"
-        ;;
-    *)
-        alias vi=vim
+#         ;;
+#   *)
+#       alias vi=vim
 #       export MANPAGER="col -b | view  -c 'setspool ft=man nomod nolist' -"
-        ;;
-esac
-
-# [ -f ~/auth/webco.bfa ] && alias auth='vim ~/auth/webco.bfa'
+#       ;;
+# esac
 # }
+
+#
+# Neovim
+#   :echo stdpath('config')
+#
+# vim-plug
+#   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#
+# Neovim: init.vim
+# call plug#begin(stdpath('data') . '/plugged')
+#   Plug 'tpope/vim-surround'
+#   Plug 'tpope/vim-repeat'
+#
+#   Plug 'tpope/vim-unimpaired'
+#   Plug 'tpope/vim-characterize'
+#
+#   Plug 'tpope/vim-commentary'
+# call plug#end()
+#
